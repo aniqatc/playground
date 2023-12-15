1f3ae6ff6d41fdfd84ad415af2b9f17a0e7a4dbb
aniqatc <aniqatc97@gmail.com>
2023-12-14 22:48:22 -0500

    organize likehandler into its own file + minor edits

--- client/src/scripts/index.js
+++ client/src/scripts/index.js
@@ -1,31 +1,7 @@
 import "../styles/main.css";
 import { loadWidgets } from "./load";
 import { applySavedTheme } from "./ui/themeHandler";
+import "./ui/likeHandler";
 
 applySavedTheme();
 loadWidgets();
-
-const likeButtons = document.querySelectorAll(".js-like-btn");
-const likeValues = document.querySelectorAll(".js-like-value");
-
-likeButtons.forEach((btn) => {
-  btn.addEventListener("click", (event) => {
-    const likesEl = event.target.parentElement.nextElementSibling;
-    const likesId = likesEl.id.split("-")[1];
-
-    handleLikes(likesEl, likesId, "POST");
-  });
-});
-
-likeValues.forEach((el) => {
-  const id = el.id.split("-")[1];
-  handleLikes(el, id, "GET");
-});
-
-async function handleLikes(el, id, type) {
-  const likesURL = `https://data.playground.aniqa.dev/widget/likes/${id}`;
-  const response = await fetch(likesURL, { method: type });
-  const data = await response.json();
-
-  el.textContent = data.likeCount;
-}
---
+++ client/src/scripts/ui/likeHandler.js
@@ -0,0 +1,24 @@
+const likeValues = document.querySelectorAll(".js-like-value");
+const likeButtons = document.querySelectorAll(".js-like-btn");
+
+likeValues.forEach((el) => {
+  const id = el.id.split("-")[1];
+  handleLikes(el, id, "GET");
+});
+
+likeButtons.forEach((btn) => {
+  btn.addEventListener("click", (event) => {
+    const likesEl = event.target.parentElement.nextElementSibling;
+    const likesId = likesEl.id.split("-")[1];
+
+    handleLikes(likesEl, likesId, "POST");
+  });
+});
+
+async function handleLikes(el, id, type) {
+  const likesURL = `https://data.playground.aniqa.dev/widget/likes/${id}`;
+  const response = await fetch(likesURL, { method: type });
+  const data = await response.json();
+
+  el.textContent = data.likeCount;
+}
