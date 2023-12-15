3ee56adbe176ccc37bd8b60a294824542b793e62
aniqatc <aniqatc97@gmail.com>
2023-12-14 21:46:40 -0500

    client: add script to send likes to DB + update el for user

--- client/src/scripts/index.js
+++ client/src/scripts/index.js
@@ -4,3 +4,23 @@ import { applySavedTheme } from "./ui/themeHandler";
 
 applySavedTheme();
 loadWidgets();
+
+const likeButtons = document.querySelectorAll(".js-like-btn");
+
+likeButtons.forEach((btn) => {
+  btn.addEventListener("click", (event) => {
+    const likesEl = event.target.parentElement.nextElementSibling;
+    const likesId = likesEl.id.split("-")[1];
+
+    addLikes(likesEl, likesId);
+  });
+});
+
+async function addLikes(el, id) {
+  const likesURL = `http://data.playground.aniqa.dev/widget/likes/${id}`;
+
+  const response = await fetch(likesURL, { method: "POST" });
+  const data = await response.json();
+
+  el.textContent = data.likes;
+}
