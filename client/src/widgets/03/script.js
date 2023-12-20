142bd829e664f6e7553e4e90f649c6d326d6c851
aniqatc <aniqatc97@gmail.com>
2023-12-20 02:54:03 -0500

    widget 03: use nullish coalescing operator for data object

--- client/src/widgets/03/script.js
+++ client/src/widgets/03/script.js
@@ -25,8 +25,8 @@ function updateMap(data) {
   const userMapParent = widget.querySelector(".map-box");
   const userDataGrid = widget.querySelector(".data-grid");
   const theme = localStorage.getItem("theme") || "outdoors";
-  const lat = data.locationData.lat || "33.55";
-  const lon = data.locationData.lon || "-117.77";
+  const lat = data?.locationData.lat || "33.55";
+  const lon = data?.locationData.lon || "-117.77";
   const mapURL = `https://api.mapbox.com/styles/v1/mapbox/${theme}-v11/static/pin-s+d27334(${lon},${lat})/${lon},${lat},12,0/300x300@2x?access_token=${process.env.MAPBOX_KEY}`;
 
   if (!mapURL.includes("undefined")) {
@@ -49,17 +49,19 @@ function updateContent(data) {
   const userTimezone = widget.querySelector(".user-timezone");
   const userISP = widget.querySelector(".user-isp");
   const userIP = widget.querySelector(".user-ip");
+  const timezoneFormatted = data?.locationData?.timezone.replace("_", " ");
 
   userBrowser.textContent =
-    `${data.browser} ${data.browserVersion}` || "Citrus Explorer 8.0";
-  userOS.textContent = data.os || "OrangeOS";
-  userIP.textContent = data.ip || "192.168.OJ";
-  userISP.textContent = data.locationData.isp || "SunNet";
-  userTimezone.textContent = data.locationData.timezone || "Orange Zone";
-  userLat.textContent = data.locationData.lat || "33.55";
-  userLon.textContent = data.locationData.lon || "-117.77";
-  userCity.textContent = data.locationData.city || "Orangetown";
-  userRegion.textContent = data.locationData.regionName || "Valley of Oranges";
-  userCountry.textContent = data.locationData.country || "Orange Republic";
-  userPlatform.textContent = data.platform || "OrangePad";
+    `${data?.browser} ${data?.browserVersion}` || "Citrus Explorer 8.0";
+  userOS.textContent = data?.os || "OrangeOS";
+  userIP.textContent = data?.ip || "192.168.OJ";
+  userISP.textContent = data?.locationData?.isp || "SunNet";
+  userTimezone.textContent = timezoneFormatted || "Orange Zone";
+  userLat.textContent = data?.locationData?.lat || "33.55";
+  userLon.textContent = data?.locationData?.lon || "-117.77";
+  userCity.textContent = data?.locationData?.city || "Orangetown";
+  userRegion.textContent =
+    data?.locationData?.regionName || "Valley of Oranges";
+  userCountry.textContent = data?.locationData?.country || "Orange Republic";
+  userPlatform.textContent = data?.platform || "OrangePad";
 }
