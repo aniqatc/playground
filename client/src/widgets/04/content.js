87349d6533205cfa10ea204aec4546522d82668c
aniqatc <aniqatc97@gmail.com>
2024-09-26 16:42:29 -0400

    widget 04: add markup & styling (unfinished)

--- client/src/widgets/04/content.js
+++ client/src/widgets/04/content.js
@@ -2,6 +2,7 @@ import "./style.scss";
 
 export function getMarkup() {
   return `
+      <!-- Widget 04: To-Do -->
       <section
         id="widget-04"
         class="relative rounded bg-stone-50 px-3 py-3 shadow-md dark:bg-zinc-800"
@@ -19,69 +20,159 @@ export function getMarkup() {
           <span class="like-value font-archivo" id="likes-04"></span>
         </div>
         <div class="content">
-          <div>
-            <section>
-              <textarea placeholder="What do you need to do?"></textarea>
-              <div>
-                <button>
-                  <i class="fa-solid fa-pen-to-square"></i> Priority Custom
-                  Select
-                </button>
-                <button><i class="fa-solid fa-calendar"></i> Calendar</button>
-                <button><i class="fa-solid fa-eye"></i> Add</button>
+          <div class="content-body">
+            <section class="todo-create-container">
+              <div class="todo-create-input">
+                <textarea
+                  placeholder="What do you need to do?"
+                  maxlength="150"
+                ></textarea>
+              </div>
+              <div class="todo-taskbar">
+                <div class="custom-select">
+                  <div class="selected-option" data-value="untagged">
+                    <i class="fa-solid fa-circle fa-fade untagged-color"></i>
+                    Untagged
+                    <i class="fa-solid fa-angle-down select-arrow-icon"></i>
+                  </div>
+                  <div class="options-list">
+                    <div class="option" data-value="untagged">
+                      <i class="fa-solid fa-circle untagged-color"></i> Untagged
+                    </div>
+                    <div class="option" data-value="high">
+                      <i class="fa-solid fa-circle high-color"></i> High
+                    </div>
+                    <div class="option" data-value="medium">
+                      <i class="fa-solid fa-circle medium-color"></i> Medium
+                    </div>
+                    <div class="option" data-value="low">
+                      <i class="fa-solid fa-circle low-color"></i> Low
+                    </div>
+                  </div>
+                </div>
+                <div class="tasks-btn-group">
+                  <button class="calendar-btn">
+                    <i class="fa-solid fa-calendar-days"></i>
+                  </button>
+                  <button class="add-todo-btn">
+                    <i class="fa-solid fa-plus"></i>
+                  </button>
+                </div>
               </div>
             </section>
-            <section>
-              <p>Upcoming</p>
-              <div>
-                <input type="checkbox" id="04-01" />
-                <label for="04-01">
-                  <span>
-                    <span>Lorem ipsum blah lorem mapisum</span>
-                    <span>Sept 24</span>
-                  </span>
-                  <button><i class="fa-regular fa-pen-to-square"></i></button>
-                  <button><i class="fa-regular fa-trash-can"></i></button>
-                </label>
-              </div>
-              <div>
-                <input type="checkbox" id="04-01" />
-                <label for="04-01">
-                  <span>
-                    <span>Lorem ipsum blah lorem mapisum</span>
-                    <span>Sept 24</span>
-                  </span>
-                  <button><i class="fa-regular fa-pen-to-square"></i></button>
-                  <button><i class="fa-regular fa-trash-can"></i></button>
-                </label>
-              </div>
+            <section class="todo-list-container">
+              <p class="todo-headings">Upcoming</p>
+              <ul>
+                <li class="todo-item">
+                  <input type="checkbox" id="04-01" />
+                  <label for="04-01">
+                    <div class="todo-item-detail-box">
+                      <span class="todo-item-description"
+                        >Lorem ipsum blah lorem mapisum</span
+                      >
+                      <span>
+                        <span class="todo-date"
+                          ><i class="fa-solid fa-hashtag high-color"></i> Sept
+                          24</span
+                        >
+                      </span>
+                    </div>
+                  </label>
+                  <div class="todo-item-btn-group">
+                    <button class="todo-edit-btn">
+                      <i class="fa-solid fa-pen"></i>
+                    </button>
+                    <button class="todo-save-btn">
+                      <i class="fa-regular fa-circle-check"></i>
+                    </button>
+                    <button class="todo-delete-btn">
+                      <i class="fa-regular fa-trash-can"></i>
+                    </button>
+                  </div>
+                </li>
+                <li class="todo-item">
+                  <input type="checkbox" id="04-02" />
+                  <label for="04-02">
+                    <div class="todo-item-detail-box">
+                      <span class="todo-item-description"
+                        >Lorem ipsum blah lorem mapisum</span
+                      >
+                      <span>
+                        <span class="todo-date"
+                          ><i class="fa-solid fa-hashtag low-color"></i> Oct
+                          24</span
+                        >
+                      </span>
+                    </div>
+                  </label>
+                  <div class="todo-item-btn-group">
+                    <button class="todo-edit-btn">
+                      <i class="fa-solid fa-pen"></i>
+                    </button>
+                    <button class="todo-save-btn">
+                      <i class="fa-regular fa-circle-check"></i>
+                    </button>
+                    <button class="todo-delete-btn">
+                      <i class="fa-regular fa-trash-can"></i>
+                    </button>
+                  </div>
+                </li>
+                <li class="todo-item">
+                  <input type="checkbox" id="04-03" />
+                  <label for="04-03">
+                    <div class="todo-item-detail-box">
+                      <span class="todo-item-description"
+                        >Lorem ipsum blah lorem mapisum</span
+                      >
+                      <span>
+                        <span class="todo-date"
+                          ><i class="fa-solid fa-hashtag low-color"></i> Sept
+                          29</span
+                        >
+                      </span>
+                    </div>
+                  </label>
+                  <div class="todo-item-btn-group">
+                    <button class="todo-edit-btn">
+                      <i class="fa-solid fa-pen"></i>
+                    </button>
+                    <button class="todo-save-btn">
+                      <i class="fa-regular fa-circle-check"></i>
+                    </button>
+                    <button class="todo-delete-btn">
+                      <i class="fa-regular fa-trash-can"></i>
+                    </button>
+                  </div>
+                </li>
+              </ul>
             </section>
             <section>
-              <p>Filter by Priority</p>
-              <button>Untagged</button>
-              <button>High</button>
-              <button>Medium</button>
-              <button>Low</button>
-            </section>
-            <section>
-              <p>Completed</p>
-              <div>
-                <input type="checkbox" id="04-01" checked />
-                <label for="04-01">
-                  <span>
-                    <span>Lorem ipsum blah lorem mapisum</span>
-                    <span>Sept 24</span>
-                  </span>
-                  <button><i class="fa-regular fa-trash-can"></i></button>
-                </label>
-              </div>
+              <p class="todo-headings">Completed</p>
+              <ul>
+                <li class="todo-item completed-todo-item">
+                  <input type="checkbox" id="04-05" checked disabled />
+                  <label for="04-05">
+                    <div class="todo-item-detail-box">
+                      <span class="todo-item-description"
+                        >Lorem ipsum blah lorem mapisum</span
+                      >
+                      <span>
+                        <span class="todo-date"
+                          ><i class="fa-solid fa-hashtag high-color"></i> Sept
+                          24</span
+                        >
+                      </span>
+                    </div>
+                  </label>
+                </li>
+              </ul>
             </section>
           </div>
-        </div>
-        <footer>
+        <footer class="content-footer">
           <button>Mark All As Completed</button>
           <button>Clear All</button>
         </footer>
+                </div>
       </section>
 `;
 }
--- client/src/widgets/04/script.js
+++ client/src/widgets/04/script.js
@@ -1,3 +1,52 @@
 export function initializeScript() {
-  console.log("Filler!");
+  document.addEventListener("DOMContentLoaded", () => {
+    const textarea = document.querySelector(".todo-create-input textarea");
+    const todoContainer = document.querySelector(".todo-taskbar");
+    const container = document.querySelector(".todo-create-container");
+    let isSelecting = false;
+
+    // Prevent textarea blur when clicking on custom select
+    const selectBtn = document.querySelector(".custom-select .selected-option");
+    const optionsList = document.querySelector(".custom-select .options-list");
+    const options = document.querySelectorAll(".custom-select .option");
+
+    selectBtn.addEventListener("mousedown", () => {
+      isSelecting = true;
+    });
+
+    optionsList.addEventListener("mousedown", () => {
+      isSelecting = true;
+    });
+
+    textarea.addEventListener("focus", () => {
+      todoContainer.classList.add("focused");
+      container.classList.add("focused");
+    });
+
+    textarea.addEventListener("blur", () => {
+      if (textarea.value.trim() === "" && !isSelecting) {
+        todoContainer.classList.remove("focused");
+        container.classList.remove("focused");
+      }
+      isSelecting = false; // Reset flag after blur event
+    });
+
+    // Toggle dropdown
+    selectBtn.addEventListener("click", () => {
+      optionsList.parentElement.classList.toggle("active");
+    });
+
+    // Update selected option
+    options.forEach((option) => {
+      option.addEventListener("click", () => {
+        const selectedValue = option.getAttribute("data-value");
+        const selectedText = option.innerHTML;
+        selectBtn.innerHTML = `${selectedText} <i class="fa-solid fa-angle-down"></i>`;
+        selectBtn.setAttribute("data-value", selectedValue);
+
+        // Close dropdown
+        optionsList.parentElement.classList.remove("active");
+      });
+    });
+  });
 }
