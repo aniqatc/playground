6c515d3594964923d580e010e910edaefc47d18d
aniqatc <aniqatc97@gmail.com>
2024-11-10 16:43:41 -0500

    widget 04: ensure default todos display on first load of a new user

--- client/src/widgets/04/script.js
+++ client/src/widgets/04/script.js
@@ -2,17 +2,17 @@ import { initializeCalendarEl } from "./core/calendar";
 import { initializeTextareaEl } from "./core/textarea";
 import { initializeInput } from "./core/input";
 import { displayDefaultTodos } from "./core/examples";
-import { initializeFilterTags} from "./core/filter";
+import { initializeFilterTags } from "./core/filter";
+import { createNewUser } from "../../main/scripts/user/userHandler";
 
-export function initializeScript() {
+export async function initializeScript() {
   document.addEventListener("DOMContentLoaded", async () => {
     initializeCalendarEl();
     initializeTextareaEl();
     initializeInput();
     initializeFilterTags();
 
-    if (localStorage.getItem("userId")) {
-      await displayDefaultTodos();
-    }
+    await createNewUser();
+    await displayDefaultTodos();
   });
 }
