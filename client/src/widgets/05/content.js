8beef66cdb8f90a0940f5699f78d9b8443a1fddb
aniqatc <aniqatc97@gmail.com>
2024-11-12 15:30:58 -0500

    widget 05: prelim markup & styling (menu & input)

--- client/src/widgets/05/content.js
+++ client/src/widgets/05/content.js
@@ -4,7 +4,7 @@ export function getMarkup() {
     return `<!-- Widget 05: Financial Markets -->
 <section
   id="widget-05"
-  class="relative h-min rounded border border-[#afb4bd] bg-stone-50 px-3 py-2 shadow-md dark:border-zinc-600 dark:bg-zinc-800"
+  class="relative h-min rounded bg-stone-50 px-3 py-2 shadow-md dark:border-zinc-600 dark:bg-zinc-800"
 >
   <div
     class="absolute -bottom-6 -ml-3 flex w-full justify-between gap-1 text-sm text-gray-500 dark:text-slate-400"
@@ -15,7 +15,20 @@ export function getMarkup() {
     </button>
     <span class="like-value font-archivo" id="likes-05"></span>
   </div>
-  <div class="content"></div>
+  <div class="content">
+    <section class="content-head">
+        <button class="top-btn active"><i class="fa-solid fa-star"></i> Top</button>
+        <button class="stock-btn">Stocks</button>
+        <button class="currency-btn">Currencies</button>
+    </section>
+    <section class="content-body">
+        <div class="top-container active">
+            <input type="text" placeholder="Search Company Symbol..." autocomplete="off" class="top-input" />
+        </div>
+        <div class="stock-container"></div>
+        <div class="currency-container"></div>
+    </section>
+</div>
 </section>
 `
 }
