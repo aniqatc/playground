83e7bff939cafbf6a6ae2fa1d375fcf83cb23640
aniqatc <aniqatc97@gmail.com>
2024-11-16 02:30:19 -0500

    widget 05: test the issue in prod

--- client/src/widgets/05/script.js
+++ client/src/widgets/05/script.js
@@ -4,13 +4,5 @@ import { generateStockCard } from "./core/stockCard.js";
 import { initializeMenu } from "./core/menu.js";
 
 export async function initializeScript() {
-    const stockData = await fetchMarketData();
-    const currencyData = await fetchCurrencyData();
-    initializeMenu(stockData, currencyData);
-
-    marketContext.clearLoadingMsg();
-    if (marketContext.stockCardGroup && stockData.stocks.length > 0 ) {
-        stockData.stocks.forEach((stock, index) => generateStockCard(stock, index));
-        marketContext.updateLastUpdated(stockData.lastUpdated);
-    }
+console.log("do nothing");
 }
