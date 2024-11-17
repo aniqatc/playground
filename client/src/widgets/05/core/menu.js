ffa7cee6720d3177debee906c28e47aa7267b5ac
aniqatc <aniqatc97@gmail.com>
2024-11-17 13:24:56 -0500

    widget 05: improve menu

--- client/src/widgets/05/core/menu.js
+++ client/src/widgets/05/core/menu.js
@@ -6,9 +6,9 @@ import { generateCurrencyCards } from "./currencyCard";
 const { stockButton, currencyButton, expandAllButton, stockTab, currencyTab, stockCardGroup, currencyCardGroup } = marketContext;
 
 export function initializeMenu(stockData, currencyData) {
-    expandAllButton.addEventListener("click", () => { initializeExpandBtn(stockData) });
     stockButton.addEventListener("click", () => { initializeStockBtn(stockData) });
     currencyButton.addEventListener("click", () => { initializeCurrencyBtn(currencyData) });
+    expandAllButton.addEventListener("click", () => { initializeExpandBtn(stockData) });
 }
 
 function initializeExpandBtn(stockData) {
@@ -55,8 +55,8 @@ function initializeStockBtn(stockData) {
     if (stockTab.classList.contains("hidden")) {
         currencyButton.classList.remove("active");
         stockButton.classList.add("active");
-        stockTab.classList.remove("hidden");
         currencyTab.classList.add("hidden");
+        stockTab.classList.remove("hidden");
         currencyCardGroup.innerHTML = ""; // remove any currency cards when hidden
         marketContext.updateLastUpdated(stockData.lastUpdated);
         marketContext.updateDescription("stocks");
@@ -67,9 +67,9 @@ function initializeCurrencyBtn(currencyData) {
     if (currencyTab.classList.contains("hidden")) {
         currencyButton.classList.add("active");
         stockButton.classList.remove("active");
-        currencyTab.classList.remove("hidden");
-        stockTab.classList.add("hidden");
         generateCurrencyCards(currencyData); // cards generate after the currency tab is clicked on
+        stockTab.classList.add("hidden");
+        currencyTab.classList.remove("hidden");
         marketContext.updateLastUpdated(currencyData.lastUpdated);
         marketContext.updateDescription("currencies");
     }
