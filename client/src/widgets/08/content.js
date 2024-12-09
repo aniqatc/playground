cabb20b3cffb58c8214a248e18caa877d4196504
aniqatc <aniqatc97@gmail.com>
2024-12-09 17:42:55 -0500

    widget 08: [temp] placeholder content + styling

--- client/src/widgets/08/content.js
+++ client/src/widgets/08/content.js
@@ -17,6 +17,130 @@ export function getMarkup() {
     <span class="like-value font-archivo" id="likes-08"></span>
   </div>
   <div class="content">
+    <section class="content-body">
+        <div class="bookmark-container">
+           <div class="bookmark">
+    <a class="bookmark-content" href="#">
+        <div class="bookmark-content--container">
+            <div class="bookmark-content--header">
+                <h2>Megamillions</h2>
+                <span>@megamillions</span>
+            </div>
+            <div class="bookmark-content--description">
+                <p>Mega Millions is one of America's two big jackpot games, and the only one with Match 5 prizes up to $5 million.</p>
+            </div>
+            <div class="bookmark-content--topics">#megamillions #lottery #millions #money</div>
+        </div>
+        <div class="bookmark-content--img">
+            <img src="${placeholderImage}" alt=""/>
+        </div>
+    </a>
+    <div class="bookmark-sidebar">
+        <div class="sidebar--actions">
+            <a class="sidebar--actions--likes" href="#">
+                <i class="fa-regular fa-thumbs-up"></i>
+                <span>11</span>
+            </a>
+            <a class="sidebar--actions--dislikes" href="#">
+                <i class="fa-regular fa-thumbs-down"></i>
+                <span>11</span>
+            </a>
+        </div>
+    </div>
+</div>
+<div class="bookmark">
+    <a class="bookmark-content" href="#">
+        <div class="bookmark-content--container">
+            <div class="bookmark-content--header">
+                <h2>Megamillions</h2>
+                <span>@megamillions</span>
+            </div>
+            <div class="bookmark-content--description">
+                <p>Mega Millions is one of America's two big jackpot games, and the only one with Match 5 prizes up to $5 million.</p>
+            </div>
+            <div class="bookmark-content--topics">#megamillions #lottery #millions #money</div>
+        </div>
+        <div class="bookmark-content--img">
+            <img src="${placeholderImage}" alt=""/>
+        </div>
+    </a>
+    <div class="bookmark-sidebar">
+        <div class="sidebar--actions">
+            <a class="sidebar--actions--likes" href="#">
+                <i class="fa-regular fa-thumbs-up"></i>
+                <span>11</span>
+            </a>
+            <a class="sidebar--actions--dislikes" href="#">
+                <i class="fa-regular fa-thumbs-down"></i>
+                <span>11</span>
+            </a>
+        </div>
+    </div>
+</div>
+<div class="bookmark">
+    <a class="bookmark-content" href="#">
+        <div class="bookmark-content--container">
+            <div class="bookmark-content--header">
+                <h2>Megamillions</h2>
+                <span>@megamillions</span>
+            </div>
+            <div class="bookmark-content--description">
+                <p>Mega Millions is one of America's two big jackpot games, and the only one with Match 5 prizes up to $5 million.</p>
+            </div>
+            <div class="bookmark-content--topics">#megamillions #lottery #millions #money</div>
+        </div>
+        <div class="bookmark-content--img">
+            <img src="${placeholderImage}" alt=""/>
+        </div>
+    </a>
+    <div class="bookmark-sidebar">
+        <div class="sidebar--actions">
+            <a class="sidebar--actions--likes" href="#">
+                <i class="fa-regular fa-thumbs-up"></i>
+                <span>11</span>
+            </a>
+            <a class="sidebar--actions--dislikes" href="#">
+                <i class="fa-regular fa-thumbs-down"></i>
+                <span>11</span>
+            </a>
+        </div>
+    </div>
+</div>
+<div class="bookmark">
+    <a class="bookmark-content" href="#">
+        <div class="bookmark-content--container">
+            <div class="bookmark-content--header">
+                <h2>Megamillions</h2>
+                <span>@megamillions</span>
+            </div>
+            <div class="bookmark-content--description">
+                <p>Mega Millions is one of America's two big jackpot games, and the only one with Match 5 prizes up to $5 million.</p>
+            </div>
+            <div class="bookmark-content--topics">#megamillions #lottery #millions #money</div>
+        </div>
+        <div class="bookmark-content--img">
+            <img src="${placeholderImage}" alt=""/>
+        </div>
+    </a>
+    <div class="bookmark-sidebar">
+        <div class="sidebar--actions">
+            <a class="sidebar--actions--likes" href="#">
+                <i class="fa-regular fa-thumbs-up"></i>
+                <span>11</span>
+            </a>
+            <a class="sidebar--actions--dislikes" href="#">
+                <i class="fa-regular fa-thumbs-down"></i>
+                <span>11</span>
+            </a>
+        </div>
+    </div>
+</div>
+        </div>
+    </section>
+    <section class="content-footer">
+        <input class="add-input" type="text" placeholder="Add a website to the community bookmarks...">
+        <button class="add-btn"><i class="fa-solid fa-plus"></i></button>
+    </section>
 </div>
 </section>`
 }
