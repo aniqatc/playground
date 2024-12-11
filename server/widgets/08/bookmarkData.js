7405905260b93ef88414b180e8ce674dc98330c3
aniqatc <aniqatc97@gmail.com>
2024-12-11 18:14:06 -0500

    widget 08: add headers on meta data fetching

--- server/widgets/08/bookmarkData.js
+++ server/widgets/08/bookmarkData.js
@@ -76,12 +76,15 @@ class BookmarkData {
             method: 'GET',
             headers: {
                 // Headers that bypass error on some websites
-                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36',
+                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
+                'Accept-Language': 'en-US,en;q=0.5',
+                'Referer': 'https://www.google.com',
+                'DNT': '1',
                 'Upgrade-Insecure-Requests': '1',
                 'Sec-Fetch-Dest': 'document',
                 'Sec-Fetch-Mode': 'navigate',
-                'Sec-Fetch-Site': 'none',
-                'Sec-Fetch-User': '?1',
+                'Sec-Fetch-Site': 'cross-site',
                 }
             });
         const html = await response.text();
