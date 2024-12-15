a62802629b253750921d4e0417dcf10ff9e9c55d
aniqatc <aniqatc97@gmail.com>
2024-12-14 22:13:03 -0500

    users: handle creating a new user *better*

--- client/src/main/scripts/user/userHandler.js
+++ client/src/main/scripts/user/userHandler.js
@@ -1,9 +1,11 @@
 import { v4 as uuidv4 } from 'uuid';
 
 async function createNewUser() {
-  if (!localStorage.getItem('userId')) {
-    const userId = uuidv4();
-    localStorage.setItem('userId', userId);
+    let userId = localStorage.getItem('userId');
+    if (!userId) {
+      userId = uuidv4();
+      localStorage.setItem('userId', userId);
+    }
 
     const serverURL = process.env.SERVER;
     const response = await fetch(`${serverURL}/users/create`, {
@@ -16,18 +18,6 @@ async function createNewUser() {
 
     await response.json();
     return userId;
-  }
-  return localStorage.getItem('userId');
 }
 
-async function fetchUserData() {
-  const userId = localStorage.getItem('userId');
-
-  if (userId) {
-    const serverURL = process.env.SERVER;
-    const response = await fetch(`${serverURL}/users/${userId}`);
-    return await response.json();
-  }
-}
-
-export { createNewUser, fetchUserData };
+export { createNewUser };
--- server/main/routers/userRouter.js
+++ server/main/routers/userRouter.js
@@ -2,24 +2,7 @@ const User = require('../models/userModel');
 const express = require('express');
 const router = express.Router();
 
-router.route('/:userId/').get(fetchUserDetails);
 router.post('/create', createNewUser);
-
-async function fetchUserDetails(req, res) {
-  try {
-    const userId = req.params.userId;
-    let user = await User.findOne({ userId: userId });
-    if (!user) {
-      return res.status(404).json({ message: 'User not found' });
-    }
-
-    res.status(200).json({ user });
-  } catch (error) {
-    res.status(500).json({
-      error: 'An error occurred while getting user details.',
-    });
-  }
-}
 
 async function createNewUser(req, res) {
   try {
